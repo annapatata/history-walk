8c19be804aebf30c8532325aeda171ec449ce2fe
Anna <anagnostopoulou.irini@gmai.com>
2025-11-28 18:59:00 +0200

    Added initial flash screen and login page

---
+++ HistoryWalk/lib/common/styles/spacing_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../../utils/constants/sizes.dart';
+
+class SpacingStyle {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: AppSizes.appBarHeight,
+    left: AppSizes.defaultSpace,
+    bottom: AppSizes.defaultSpace,
+    right: AppSizes.defaultSpace,
+  );
+}
---
+++ HistoryWalk/lib/features/auth/screens/login/login_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:historywalk/common/styles/spacing_styles.dart';
+import 'package:historywalk/utils/constants/text_strings.dart';
+import 'package:historywalk/utils/constants/sizes.dart';
+import 'package:historywalk/navigation_menu.dart';
+
+class LoginScreen extends StatelessWidget{
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView( //we want to make the screen scrollable for small screens
+        child: Padding(
+          padding: SpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              //title
+              Text(AppTexts.loginTitle, style: Theme.of(context).textTheme.headlineMedium),
+              //form
+              Form(child:Column(
+                children: [
+                  //Email
+                  TextFormField(
+                    decoration: const InputDecoration(prefixIcon: Icon(Icons.email_outlined) , labelText: "Email"),
+                    ),
+                    
+                  const SizedBox(height: AppSizes.spaceBtwItems),
+                  
+                  //Password
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.password_outlined), 
+                      labelText: "Password",
+                      suffixIcon: Icon(Icons.remove_red_eye_rounded),
+                      ),
+                    ),
+                  const SizedBox(height: AppSizes.spaceBtwItems / 2),
+
+                  //Remember Me & Forget Password
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+                    children: [
+                      //Remember Me
+                      Row(children: [
+                        Checkbox(value: true, onChanged: (value){}),
+                        const Text("Remember Me"),
+                      ],),
+                      
+                      //Forget Button
+                      TextButton(onPressed: (){}, child: const Text("Forget Password?")),
+                    ],
+                  ),
+                  const SizedBox(height: AppSizes.spaceBtwSections),
+
+                  //Sign In Button
+                  SizedBox(width: double.infinity, child: ElevatedButton(
+                    onPressed: () { Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const NavigationMenu()));},
+                    child: Text("Let's Go!"))),
+                ],
+              ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
