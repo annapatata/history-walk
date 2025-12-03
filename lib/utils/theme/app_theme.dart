12720dc4e496aed1272528483ba2de7e3b4f9c98
ntua-el22898 <el22898@mail.ntua.gr>
2025-12-03 12:36:40 +0200

    Delete lib directory

--- lib/utils/theme/app_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'light_theme.dart';
-import 'dark_theme.dart';
-
-class AppTheme {
-  static ThemeData get light => lightTheme;
-  static ThemeData get dark => darkTheme;
-}
