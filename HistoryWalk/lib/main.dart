99e08efaebdab5b056b40e1cb58bdd39653850b8
SOTIRIS IOANNOU <sotirisioannou1803@gmail.com>
2026-01-05 19:18:17 +0200

    fixed map, δειτε αν δουλευει

--- HistoryWalk/lib/main.dart
+++ HistoryWalk/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:historywalk/app.dart';
-import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+// import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -13,9 +13,9 @@ void main() async {
   // Init GetStorage
   await GetStorage.init();
 
-  // Mapbox token
-  String token = const String.fromEnvironment("ACCESS_TOKEN");
-  MapboxOptions.setAccessToken(token);
+  // // Mapbox token
+  // String token = const String.fromEnvironment("ACCESS_TOKEN");
+  // MapboxOptions.setAccessToken(token);
 
   runApp(const App());
 }
